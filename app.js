b20ad77f4216b01064f3199424a39b3ec20d317d
Tanvir Imam Mitul <blackpanther141532@gmail.com>
2023-01-30 17:55:31 +0600

    mailchimp implementation

---
+++ app.js
@@ -0,0 +1,72 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require("request")
+const https = require("https")
+const app = express()
+const port = 3000
+
+app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+app.use(express.static("public"))
+
+
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/signup.html`)
+  })
+
+
+
+app.post('/', (req, res) => {
+    
+  const fName = req.body.firstname ;
+  const lName = req.body.lastname ;
+  const email = req.body.email ;
+  
+  const data ={
+    members:[
+      {
+        email_address:email,
+        status:"subscribed",
+        merge_fields: {
+          FNAME: fName,
+          LNAME:lName
+        }
+      }
+    ]
+  };
+
+  const jsonData = JSON.stringify(data) ;
+
+  const url = 'https://us9.api.mailchimp.com/3.0/lists/4881d9802d' ;
+  
+  const option ={
+    method:"POST",
+    auth: "tanvirmitul:a0aeb2d1534644a6929ad26334ece244-us9"
+  }
+
+  const request = https.request(url,option,(response)=>{
+    response.on("data", (data)=>{
+      console.log(JSON.parse(data));
+
+    })
+
+  })
+
+  request.write(jsonData);
+  request.end() ;
+
+
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+
+
+//api keys
+//a0aeb2d1534644a6929ad26334ece244-us9
+
+
+//list id
+//4881d9802d 
